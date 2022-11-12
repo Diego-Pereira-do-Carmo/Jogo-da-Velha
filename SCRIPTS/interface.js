a7fe1549a5251973dd935c2d1c3f276ff0a19a3f
unknown <carmopereiradiego@gmail.com>
2022-11-12 17:49:32 -0300

    end of the first version

--- SCRIPTS/interface.js
+++ SCRIPTS/interface.js
@@ -11,23 +11,19 @@ function handleClick(event) {
   let square = event.target;
   let position = square.id;
 
-  handleMove(position);
+  if (handleMove(position)) {
 
-  updateSquares();
+    setTimeout(() => {
+      alert("O jogo Acabou - O Jogador " + (playerTime + 1) + " Foi o Vencedor");
+    }, 10);
+
+  };
+
+  updateSquare(position);
 }
 
-function updateSquares() {
-
-  let squares = document.querySelectorAll(".square");
-
-  squares.forEach((square) => {
-    let position = square.id;
-    let symbol = board[position];
-
-    if (symbol != '') {
-      square.innerHTML = `<div class='${symbol}'></div>`
-    }
-  })
+function updateSquare(position){
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`
 }
-
-
